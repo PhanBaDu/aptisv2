c02531e370f0cca97a83f4b20ab3e4ab0ec6788c
Du <119209610+PhanBaDu@users.noreply.github.com>
2026-06-29 16:36:47 +0700

    Initial Commit

---
+++ aptisv2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aptisv2
+//
+//  Created by Phan Ba Du on 29/6/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aptisv2/aptisv2App.swift
@@ -0,0 +1,17 @@
+//
+//  aptisv2App.swift
+//  aptisv2
+//
+//  Created by Phan Ba Du on 29/6/26.
+//
+
+import SwiftUI
+
+@main
+struct aptisv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
